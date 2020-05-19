7317c6e039d5e6fdf2e426f6e009785f899fe9e3
Liny927 <52920114+Liny927@users.noreply.github.com>
2020-05-19 23:44:39 +0800

    Update ex13_31.h

--- ch13/ex13_31.h
+++ ch13/ex13_31.h
@@ -1,59 +1,83 @@
 //
-//  ex13_31.h
-//  Exercise 13.31
+// Created by hellcat on 2020.05.18.
 //
-//  Created by pezy on 1/23/15.
-//  Copyright (c) 2015 pezy. All rights reserved.
-//
-//  Give your class a < operator and define a vector of HasPtrs.
-//  Give that vector some elements and then sort the vector.
-//  Note when swap is called.
-//
-//  See ex13_30.h
-
-#ifndef CP5_ex13_11_h
-#define CP5_ex13_11_h
-
-#include <string>
 #include <iostream>
+#include <vector>
+#include <string>
+using std::cout;
+using std::endl;
+using std::string;
 
 class HasPtr {
 public:
-    friend void swap(HasPtr&, HasPtr&);
-    friend bool operator<(const HasPtr& lhs, const HasPtr& rhs);
-    HasPtr(const std::string& s = std::string()) : ps(new std::string(s)), i(0)
-    {
+    explicit HasPtr(const string &s = string())
+        : ps(new string(s)), i(0), use(new size_t(1)) {}
+    HasPtr(const HasPtr& rhs)
+        : ps(new string(*rhs.ps)), i(rhs.i), use(rhs.use) { ++*use; }
+    ~HasPtr() {
+        if(--*use == 0) {
+            delete ps;
+            delete use;
+        }
     }
-    HasPtr(const HasPtr& hp) : ps(new std::string(*hp.ps)), i(hp.i) {}
-    HasPtr& operator=(HasPtr tmp)
-    {
+    friend void swap(HasPtr &lhs, HasPtr &rhs);
+    bool operator < (const HasPtr &rhs) {
+        return *ps < *rhs.ps;
+    }
+
+    HasPtr& operator = (HasPtr tmp) {
+//        ++*rhs_hp.use;
+//        if(--*use == 0) {
+//            delete ps;
+//            delete use;
+//        }
+//        ps = rhs_hp.ps;
+//        i = rhs_hp.i;
+//        use = rhs_hp.use;
+//        return *this;
         this->swap(tmp);
         return *this;
     }
-    ~HasPtr() { delete ps; }
-
-    void swap(HasPtr& rhs)
-    {
+    void swap(HasPtr& rhs) {
         using std::swap;
         swap(ps, rhs.ps);
         swap(i, rhs.i);
-        std::cout << "call swap(HasPtr &rhs)" << std::endl;
+        cout<<"swap(HasPtr& rhs) called."<<endl;
     }
 
-    void show() const { std::cout << *ps << std::endl; }
+    void print() {
+//        cout<<(*use)<<endl;
+        cout<<*ps<<endl;
+    }
+
 private:
-    std::string* ps;
+    string *ps;
     int i;
+    size_t *use; // 记录有多少个对象共享*ps的成员
 };
 
-void swap(HasPtr& lhs, HasPtr& rhs)
-{
-    lhs.swap(rhs);
+void swap(HasPtr &lhs, HasPtr &rhs) {
+    using std::swap;
+    swap(lhs.ps, rhs.ps);
+    swap(lhs.i, rhs.i);
+    cout<<"swap(HasPtr &lhs, HasPtr &rhs) called."<<endl;
 }
+#include <vector>
+#include <algorithm>
+using std::vector;
+using std::sort;
 
-bool operator<(const HasPtr& lhs, const HasPtr& rhs)
-{
-    return *lhs.ps < *rhs.ps;
+int main() {
+    HasPtr p1("abc"), p2("abcd"), p3("bca");
+//    HasPtr p2(p1);
+//    HasPtr p3(p1);
+//    HasPtr p4;
+//
+//    p3.print();
+//    p4 = p2;  // swap(HasPtr &lhs, HasPtr &rhs) called.
+//    p4.print();
+    vector<HasPtr> v = { p3 };
+    v.push_back(p1);
+    v.push_back(p2);
+    sort(v.begin(), v.end()); // 交换4次
 }
-
-#endif
