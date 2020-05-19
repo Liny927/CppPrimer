5e670e8d8812109d7478ad6a82b4baa2e2ca2dad
Liny927 <52920114+Liny927@users.noreply.github.com>
2020-05-19 14:01:53 +0800

    Update ex13_13.cpp

--- ch13/ex13_13.cpp
+++ ch13/ex13_13.cpp
@@ -20,35 +20,68 @@
 //  copy-control member is used.
 //  As you read the output, remember that the compiler can omit calls to the
 //  copy constructor.
-
+//
+// Created by hellcat on 2020.05.18.
+//
 #include <iostream>
 #include <vector>
-#include <initializer_list>
+using namespace std;
 
 struct X {
-    X() { std::cout << "X()" << std::endl; }
-    X(const X&) { std::cout << "X(const X&)" << std::endl; }
-    X& operator=(const X&)
-    {
-        std::cout << "X& operator=(const X&)" << std::endl;
-        return *this;
-    }
-    ~X() { std::cout << "~X()" << std::endl; }
+    X() { cout<<"X() called."<<endl; }
+    ~X() { cout<<"~X() called."<<endl; }
+    X(const X&) { cout<<"X(const X&) called."<<endl; }
+    X& operator = (const X&) { cout<<"X& operator = (const X&) called."<<endl; }
+    int isbn() { return a; }
+    int a;
 };
 
-void f(const X& rx, X x)
+bool fcn(const X* trans, X accum)
 {
-    std::vector<X> vec;
-    vec.reserve(2);
-    vec.push_back(rx);
-    vec.push_back(x);
+    X item1(*trans), item2(accum);
+    cout<<"------------------------"<<endl;
+    return item1.isbn() != item2.isbn();
+    // accum(非引用参数传递, 临时变量), item1, item2离开局部作用域, 被销毁
+    // trans为指针传递, 不被销毁
 }
 
-int main()
-{
-    X* px = new X;
-    f(*px, *px);
-    delete px;
+void f(const X &rx, X x) { // X(const X&) called.
+    vector<X> vec;
+    vec.reserve(2);
+    vec.push_back(rx); // X(const X&) called.
+    cout<<"*****"<<endl;
+    vec.push_back(x);  // X(const X&) called. 
+    cout<<"*****"<<endl;
+    // ~X() called.
+    // ~X() called.
+    // ~X() called.
+}
 
-    return 0;
+void f(const X &rx) {
+    vector <X> vec;
+    vec.push_back(rx); // X(const X&) called.
+    // ~X() called.
+}
+
+void f2(X x) { // X(const X&) called.
+    vector <X> vec;
+    cout<<"*****"<<endl;
+    vec.push_back(x); // X(const X&) called.
+    cout<<"*****"<<endl;
+//     ~X() called.
+//     ~X() called.
+}
+
+int main() {
+//    X x1, x2;
+//    cout<<"-----------"<<endl;
+//    fcn(&x1, x2);
+//    cout<<"-----------"<<endl;
+    X *px = new X;
+    cout<<"-----------"<<endl;
+    f(*px, *px);
+//    f(*px);
+//    f2(*px);
+    cout<<"-----------"<<endl;
+    delete px;
 }
